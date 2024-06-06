5bc126d09070b95b416df68d7c10e8e26a7b5aab
Felipe Atamanczuk <felipe.atamanczuk0@gmail.com>
2024-06-05 22:45:45 -0300

    UsuarioDAO

---
+++ UsuarioDAO.java
@@ -0,0 +1,58 @@
+
+/**
+ *
+ * @author Felipe Atamanczuk
+ */
+public class UsuarioDAO {
+    //tipo de retorno, nome, lista de parametros, corpo
+   public Usuario existe(Usuario u) throws Exception {
+        //1. Especificar o comando SQL (SELECT)
+        var sql = "SELECT * FROM tb_usuarios WHERE login = ? AND  senha = ?";
+        //2. Estabelecer uma conexão com o banco
+        var conexao = new ConnectionFactory().conectar();
+        //3. Preparar o comando
+        var ps = conexao.prepareStatement(sql);
+        //4. Substituir os eventuais placeholders
+        ps.setString(1, u.getLogin());
+        ps.setString(2, u.getSenha());
+        //5. Executar o comando
+        var rs = ps.executeQuery();
+        //6. Tratar o resultado e devolver true ou false
+        var usuarioExiste = rs.next();
+        if(usuarioExiste){
+            var tipo = rs.getInt("tipo");
+            u.setTipo(tipo);
+        }
+        else{
+            u = null;
+        }
+        //7. Fechar a conexão
+        rs.close();
+        ps.close();
+        conexao.close();
+//        if(usuarioExiste)
+//            return true;
+//        return false;
+//        return usuarioExiste ? true : false;
+          return u;
+    }
+            
+    //cláusula catch or declare
+   public void cadastrar(Usuario u)  throws Exception{
+        //1. Especificar o comando SQL (INSERT)
+        var sql = "INSERT INTO tb_usuarios(login, senha, tipo) VALUES(?, ?)";
+        //2. Estabelecer uma conexão com o banco
+        var conexao = new ConnectionFactory().conectar();
+        //3. Preparar o comando
+        var ps = conexao.prepareStatement(sql);
+        //4. Substituir os eventuais placeholders
+        ps.setString(1, u.getLogin());
+        ps.setString(2, u.getSenha());
+//        ps.setInt(3, );
+        //5. Executar o comando
+        ps.execute();
+        //6. Fechar a conexão
+        conexao.close();
+    }     
+}
+
