6d5567d41826992d38bf022bdc2d2bf992d617e0
Felipe Atamanczuk <felipe.atamanczuk0@gmail.com>
2024-06-05 21:38:09 -0300

    Script banco de dados

---
+++ Bancodedado.sql
@@ -0,0 +1,12 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  Felipe Atamanczuk
+ * Created: 28 de abr. de 2024
+ */
+SELECT * FROM tb_eventos;
+
+
+
